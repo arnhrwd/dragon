0c6bb21249b1ac2fc112ad8d1042c6afe639d295
GayashanNA <gayashan.amarasinghe@gmail.com>
2019-09-12 23:17:43 +1000

    Fixed serialisation issue with the object array of values in Fields

--- src/main/java/dragon/tuple/Fields.java
+++ src/main/java/dragon/tuple/Fields.java
@@ -6,31 +6,31 @@ import java.util.HashMap;
 
 public class Fields implements Serializable, Cloneable {
 	/**
-	 * 
+	 *
 	 */
 	private static final long serialVersionUID = -134149710944581963L;
-	private transient Object[] values;
+	private Object[] values;
 	private HashMap<String,Integer> fieldMap;
 	private String[] fieldNames;
-	
+
 	public Fields(String...fieldNames) {
 		this.fieldNames=fieldNames;
 		fieldMap = new HashMap<String,Integer>();
 		values = new Object[fieldNames.length];
 		for(int i=0;i<fieldNames.length;i++) {
-			values[i]=new Object();
+			values[i] = null;
 			fieldMap.put(fieldNames[i], i);
 		}
 	}
-	
+
 	public Object get(int i) {
 		return values[i];
 	}
-	
+
 	public void set(int i,Object value) {
 		values[i]=value;
 	}
-	
+
 	public HashMap<String,Integer> getFieldMap(){
 		return fieldMap;
 	}
@@ -39,15 +39,15 @@ public class Fields implements Serializable, Cloneable {
 		Fields f = new Fields(this.fieldNames);
 		return f;
 	}
-	
+
 	public Object[] getValues() {
 		return values;
 	}
-	
+
 	public String[] getFieldNames() {
 		return fieldNames;
 	}
-	
+
 	public String getFieldNamesAsString() {
 		String names="<";
 		for(String name : fieldNames) {
@@ -55,7 +55,7 @@ public class Fields implements Serializable, Cloneable {
 		}
 		return names+">";
 	}
-	
+
 	public int size(){
 		return fieldNames.length;
 	}
