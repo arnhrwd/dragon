a2c856491fcc92f54e9c48e70f2896a7597a9e6e
Aaron Harwood <aharwood@unimelb.edu.au>
2019-10-26 19:42:53 +1100

    debug statements

--- src/main/java/dragon/tuple/Recycler.java
+++ src/main/java/dragon/tuple/Recycler.java
@@ -22,7 +22,7 @@ public class Recycler<T> {
 //	private AtomicReferenceArray<T> objects;
 //	private int next;
 //	private final HashMap<T,Integer> map;
-	private CircularBlockingQueue<T> objects;
+	private volatile CircularBlockingQueue<T> objects;
 	private final Map<T,AtomicInteger> refCount;
 	private final ReentrantLock lock = new ReentrantLock();
 		
@@ -52,18 +52,38 @@ public class Recycler<T> {
 		lock.lock();
 		try {
 			objects.put(t);
-			if(objects.remainingCapacity()>expansion &&
+			if(objects.size()>expansion &&
 					objects.remainingCapacity()<objects.getCapacity()*compact) {
 				final int newCapacity = (int)(objects.getCapacity()*0.5);
-				log.warn(this.getClass().getName()+" shrinking to "+(newCapacity));
+				final int c1 = objects.remainingCapacity();
+				log.debug("used objects = "+c1);
+				log.warn(obj.getClass().getName()+" shrinking to "+(newCapacity));
 				CircularBlockingQueue<T> newObjects = new CircularBlockingQueue<T>(newCapacity);
+				log.debug("objects size = "+objects.size());
 				for(int i=0;i<newCapacity;i++) {
-					newObjects.put(objects.take()); // ref counts are retained in refCount
+					T t1 = objects.take();
+					if(refCount.containsKey(t1)) {
+						if(refCount.get(t1).get()!=0) {
+							System.out.println("error");
+							System.exit(-1);
+						}
+					} else {
+						System.out.println("error2");
+						System.exit(-1);
+					}
+					newObjects.put(t1); // ref counts are retained in refCount
 				}
+				log.debug("objects size = "+objects.size());
 				while(objects.size()>0) {
 					refCount.remove(objects.take()); // remove these unneeded ref counts
 				}
+				log.debug("objects size = "+objects.size());
 				objects=newObjects;
+				log.debug("finished shrinking");
+				if(c1<objects.remainingCapacity()) {
+					System.out.println("error3");
+					System.exit(-1);
+				}
 			}
 		} catch (InterruptedException e) {
 			log.error("could not recycle object");
@@ -80,7 +100,7 @@ public class Recycler<T> {
 			T t = objects.poll();
 			if(t==null) {
 				final int capacity = objects.getCapacity();
-				log.warn(this.getClass().getName()+" expanding by "+expansion+" to "+(capacity+expansion));
+				log.warn(obj.getClass().getName()+" expanding by "+expansion+" to "+(capacity+expansion));
 				this.objects = new CircularBlockingQueue<T>(capacity+expansion);
 				for(int i=0;i<expansion;i++) {
 					T tp = (T) ((IRecyclable)obj).newRecyclable();
@@ -105,10 +125,18 @@ public class Recycler<T> {
 	}
 
 	public void shareRecyclable(T t,int n) {
+		if(!refCount.containsKey(t)) {
+			System.out.println("error4");
+			System.exit(-1);
+		}
 		refCount.get(t).addAndGet(n);
 	}
 
 	public void crushRecyclable(T t,int n) {
+		if(!refCount.containsKey(t)) {
+			System.out.println("error5");
+			System.exit(-1);
+		}
 		long c = refCount.get(t).addAndGet(-n);
 		if(c==0) {
 			recycleObject(t);
